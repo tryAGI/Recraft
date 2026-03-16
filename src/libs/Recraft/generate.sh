03e759a9e7113088629b1acdbc086bcdb1e98973
Konstantin S. <havendv@gmail.com>
2026-03-16 21:56:01 +0400

    Remove FixOpenApiSpec helper

--- src/libs/Recraft/generate.sh
+++ src/libs/Recraft/generate.sh
@@ -1,11 +1,6 @@
 dotnet tool install --global autosdk.cli --prerelease
 rm -rf Generated
 curl -o openapi.yaml https://external.api.recraft.ai/doc/spec/api.yaml
-dotnet run --project ../../helpers/FixOpenApiSpec openapi.yaml
-if [ $? -ne 0 ]; then
- echo "Failed, exiting..."
- exit 1
-fi
 autosdk generate openapi.yaml \
   --namespace Recraft \
   --clientClassName RecraftClient \

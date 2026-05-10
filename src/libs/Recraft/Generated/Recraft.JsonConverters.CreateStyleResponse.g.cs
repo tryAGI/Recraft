699059eb48456ce7176afde34d249200a3d786e2
Konstantin S. <havendv@gmail.com>
2026-05-10 07:35:10 +0400

    feat: Updated OpenAPI spec (#134)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Recraft/Generated/Recraft.JsonConverters.CreateStyleResponse.g.cs
+++ src/libs/Recraft/Generated/Recraft.JsonConverters.CreateStyleResponse.g.cs
@@ -80,6 +80,7 @@ namespace Recraft.JsonConverters
             {
                 try
                 {
+
                     var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Recraft.Style), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Recraft.Style> ??
                                    throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Recraft.Style).Name}");
                     style = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
@@ -90,9 +91,13 @@ namespace Recraft.JsonConverters
                 catch (global::System.InvalidOperationException)
                 {
                 }
+            }
 
+            if (style == null && createStyleResponseVariant2 == null)
+            {
                 try
                 {
+
                     var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Recraft.CreateStyleResponseVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Recraft.CreateStyleResponseVariant2> ??
                                    throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Recraft.CreateStyleResponseVariant2).Name}");
                     createStyleResponseVariant2 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);

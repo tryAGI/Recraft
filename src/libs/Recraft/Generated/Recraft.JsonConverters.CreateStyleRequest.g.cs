aa3822add1293e3524031066857d9673795d021e
Konstantin S. <havendv@gmail.com>
2026-08-29 05:32:22 +0400

    feat: Updated OpenAPI spec (#164)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

--- src/libs/Recraft/Generated/Recraft.JsonConverters.CreateStyleRequest.g.cs
+++ src/libs/Recraft/Generated/Recraft.JsonConverters.CreateStyleRequest.g.cs
@@ -17,113 +17,34 @@ namespace Recraft.JsonConverters
 
             using var __jsonDocument = global::System.Text.Json.JsonDocument.ParseValue(ref reader);
             var __rawJson = __jsonDocument.RootElement.GetRawText();
-            var __jsonProps = new global::System.Collections.Generic.HashSet<string>();
-            if (__jsonDocument.RootElement.ValueKind == global::System.Text.Json.JsonValueKind.Object)
-            {
-                foreach (var __jsonProp in __jsonDocument.RootElement.EnumerateObject())
-                {
-                    __jsonProps.Add(__jsonProp.Name);
-                    if (__jsonProp.Value.ValueKind == global::System.Text.Json.JsonValueKind.Object)
-                    {
-                        foreach (var __nestedJsonProp in __jsonProp.Value.EnumerateObject())
-                        {
-                            __jsonProps.Add(__jsonProp.Name + "." + __nestedJsonProp.Name);
-                        }
-                    }
-
-                }
-            }
-
-            var __score0 = 0;
-            if (__jsonProps.Contains("image_weights")) __score0++;
-            if (__jsonProps.Contains("match")) __score0++;
-            if (__jsonProps.Contains("mix_policy")) __score0++;
-            if (__jsonProps.Contains("model")) __score0++;
-            if (__jsonProps.Contains("palette")) __score0++;
-            if (__jsonProps.Contains("palette.background_color")) __score0++;
-            if (__jsonProps.Contains("palette.colors")) __score0++;
-            if (__jsonProps.Contains("private")) __score0++;
-            if (__jsonProps.Contains("prompt")) __score0++;
-            if (__jsonProps.Contains("source_style_weights")) __score0++;
-            if (__jsonProps.Contains("source_styles")) __score0++;
-            if (__jsonProps.Contains("style")) __score0++;
-            var __score1 = 0;
-            if (__jsonProps.Contains("images")) __score1++;
-            var __bestScore = 0;
-            var __bestIndex = -1;
-            if (__score0 > __bestScore) { __bestScore = __score0; __bestIndex = 0; }
-            if (__score1 > __bestScore) { __bestScore = __score1; __bestIndex = 1; }
 
             global::Recraft.CreateStyleRequestBase? @base = default;
+            try
+            {
+                var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Recraft.CreateStyleRequestBase), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Recraft.CreateStyleRequestBase> ??
+                               throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Recraft.CreateStyleRequestBase).Name}");
+                @base = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
+            }
+            catch (global::System.Text.Json.JsonException)
+            {
+            }
+            catch (global::System.InvalidOperationException)
+            {
+            }
+
             global::Recraft.CreateStyleRequestVariant2? createStyleRequestVariant2 = default;
-            if (__bestIndex >= 0)
+            try
             {
-                if (__bestIndex == 0)
-                {
-                    try
-                    {
-                        var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Recraft.CreateStyleRequestBase), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Recraft.CreateStyleRequestBase> ??
-                                       throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Recraft.CreateStyleRequestBase).Name}");
-                        @base = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                    }
-                    catch (global::System.Text.Json.JsonException)
-                    {
-                    }
-                    catch (global::System.InvalidOperationException)
-                    {
-                    }
-                }
-                else if (__bestIndex == 1)
-                {
-                    try
-                    {
-                        var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Recraft.CreateStyleRequestVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Recraft.CreateStyleRequestVariant2> ??
-                                       throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Recraft.CreateStyleRequestVariant2).Name}");
-                        createStyleRequestVariant2 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                    }
-                    catch (global::System.Text.Json.JsonException)
-                    {
-                    }
-                    catch (global::System.InvalidOperationException)
-                    {
-                    }
-                }
+                var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Recraft.CreateStyleRequestVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Recraft.CreateStyleRequestVariant2> ??
+                               throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Recraft.CreateStyleRequestVariant2).Name}");
+                createStyleRequestVariant2 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
             }
-
-            if (@base == null && createStyleRequestVariant2 == null)
+            catch (global::System.Text.Json.JsonException)
             {
-                try
-                {
-
-                    var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Recraft.CreateStyleRequestBase), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Recraft.CreateStyleRequestBase> ??
-                                   throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Recraft.CreateStyleRequestBase).Name}");
-                    @base = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                }
-                catch (global::System.Text.Json.JsonException)
-                {
-                }
-                catch (global::System.InvalidOperationException)
-                {
-                }
             }
-
-            if (@base == null && createStyleRequestVariant2 == null)
+            catch (global::System.InvalidOperationException)
             {
-                try
-                {
-
-                    var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Recraft.CreateStyleRequestVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Recraft.CreateStyleRequestVariant2> ??
-                                   throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Recraft.CreateStyleRequestVariant2).Name}");
-                    createStyleRequestVariant2 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                }
-                catch (global::System.Text.Json.JsonException)
-                {
-                }
-                catch (global::System.InvalidOperationException)
-                {
-                }
             }
-
             var __value = new global::Recraft.CreateStyleRequest(
                 @base,
 
@@ -142,18 +63,46 @@ namespace Recraft.JsonConverters
             options = options ?? throw new global::System.ArgumentNullException(nameof(options));
             var typeInfoResolver = options.TypeInfoResolver ?? throw new global::System.InvalidOperationException("TypeInfoResolver is not set.");
 
+
+            writer.WriteStartObject();
+            var __writtenPropertyNames = new global::System.Collections.Generic.HashSet<string>(global::System.StringComparer.Ordinal);
             if (value.IsBase)
             {
                 var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Recraft.CreateStyleRequestBase), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Recraft.CreateStyleRequestBase?> ??
                                throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Recraft.CreateStyleRequestBase).Name}");
-                global::System.Text.Json.JsonSerializer.Serialize(writer, value.Base!, typeInfo);
+                var __element0 = global::System.Text.Json.JsonSerializer.SerializeToElement(value.Base!, typeInfo);
+                if (__element0.ValueKind != global::System.Text.Json.JsonValueKind.Object)
+                {
+                    throw new global::System.Text.Json.JsonException("AllOf values must serialize as JSON objects.");
+                }
+
+                foreach (var __property in __element0.EnumerateObject())
+                {
+                    if (__writtenPropertyNames.Add(__property.Name))
+                    {
+                        __property.WriteTo(writer);
+                    }
+                }
             }
-            else if (value.IsCreateStyleRequestVariant2)
+            if (value.IsCreateStyleRequestVariant2)
             {
                 var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Recraft.CreateStyleRequestVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Recraft.CreateStyleRequestVariant2?> ??
                                throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Recraft.CreateStyleRequestVariant2).Name}");
-                global::System.Text.Json.JsonSerializer.Serialize(writer, value.CreateStyleRequestVariant2!, typeInfo);
+                var __element1 = global::System.Text.Json.JsonSerializer.SerializeToElement(value.CreateStyleRequestVariant2!, typeInfo);
+                if (__element1.ValueKind != global::System.Text.Json.JsonValueKind.Object)
+                {
+                    throw new global::System.Text.Json.JsonException("AllOf values must serialize as JSON objects.");
+                }
+
+                foreach (var __property in __element1.EnumerateObject())
+                {
+                    if (__writtenPropertyNames.Add(__property.Name))
+                    {
+                        __property.WriteTo(writer);
+                    }
+                }
             }
+            writer.WriteEndObject();
         }
     }
 }
--- src/libs/Recraft/Generated/Recraft.JsonConverters.GenerateImageRequest.g.cs
+++ src/libs/Recraft/Generated/Recraft.JsonConverters.GenerateImageRequest.g.cs
@@ -17,124 +17,34 @@ namespace Recraft.JsonConverters
 
             using var __jsonDocument = global::System.Text.Json.JsonDocument.ParseValue(ref reader);
             var __rawJson = __jsonDocument.RootElement.GetRawText();
-            var __jsonProps = new global::System.Collections.Generic.HashSet<string>();
-            if (__jsonDocument.RootElement.ValueKind == global::System.Text.Json.JsonValueKind.Object)
-            {
-                foreach (var __jsonProp in __jsonDocument.RootElement.EnumerateObject())
-                {
-                    __jsonProps.Add(__jsonProp.Name);
-                    if (__jsonProp.Value.ValueKind == global::System.Text.Json.JsonValueKind.Object)
-                    {
-                        foreach (var __nestedJsonProp in __jsonProp.Value.EnumerateObject())
-                        {
-                            __jsonProps.Add(__jsonProp.Name + "." + __nestedJsonProp.Name);
-                        }
-                    }
-
-                }
-            }
-
-            var __score0 = 0;
-            if (__jsonProps.Contains("block_nsfw")) __score0++;
-            if (__jsonProps.Contains("calculate_features")) __score0++;
-            if (__jsonProps.Contains("controls")) __score0++;
-            if (__jsonProps.Contains("controls.artistic_level")) __score0++;
-            if (__jsonProps.Contains("controls.background_color")) __score0++;
-            if (__jsonProps.Contains("controls.colors")) __score0++;
-            if (__jsonProps.Contains("controls.no_text")) __score0++;
-            if (__jsonProps.Contains("creativity")) __score0++;
-            if (__jsonProps.Contains("expire")) __score0++;
-            if (__jsonProps.Contains("image_format")) __score0++;
-            if (__jsonProps.Contains("model")) __score0++;
-            if (__jsonProps.Contains("n")) __score0++;
-            if (__jsonProps.Contains("negative_prompt")) __score0++;
-            if (__jsonProps.Contains("prompt")) __score0++;
-            if (__jsonProps.Contains("random_seed")) __score0++;
-            if (__jsonProps.Contains("response_format")) __score0++;
-            if (__jsonProps.Contains("size")) __score0++;
-            if (__jsonProps.Contains("style")) __score0++;
-            if (__jsonProps.Contains("style_id")) __score0++;
-            if (__jsonProps.Contains("style_match")) __score0++;
-            if (__jsonProps.Contains("substyle")) __score0++;
-            if (__jsonProps.Contains("text_layout")) __score0++;
-            if (__jsonProps.Contains("upscale")) __score0++;
-            var __score1 = 0;
-            if (__jsonProps.Contains("style_references")) __score1++;
-            var __bestScore = 0;
-            var __bestIndex = -1;
-            if (__score0 > __bestScore) { __bestScore = __score0; __bestIndex = 0; }
-            if (__score1 > __bestScore) { __bestScore = __score1; __bestIndex = 1; }
 
             global::Recraft.GenerateImageRequestBase? @base = default;
+            try
+            {
+                var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Recraft.GenerateImageRequestBase), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Recraft.GenerateImageRequestBase> ??
+                               throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Recraft.GenerateImageRequestBase).Name}");
+                @base = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
+            }
+            catch (global::System.Text.Json.JsonException)
+            {
+            }
+            catch (global::System.InvalidOperationException)
+            {
+            }
+
             global::Recraft.GenerateImageRequestVariant2? generateImageRequestVariant2 = default;
-            if (__bestIndex >= 0)
+            try
             {
-                if (__bestIndex == 0)
-                {
-                    try
-                    {
-                        var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Recraft.GenerateImageRequestBase), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Recraft.GenerateImageRequestBase> ??
-                                       throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Recraft.GenerateImageRequestBase).Name}");
-                        @base = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                    }
-                    catch (global::System.Text.Json.JsonException)
-                    {
-                    }
-                    catch (global::System.InvalidOperationException)
-                    {
-                    }
-                }
-                else if (__bestIndex == 1)
-                {
-                    try
-                    {
-                        var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Recraft.GenerateImageRequestVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Recraft.GenerateImageRequestVariant2> ??
-                                       throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Recraft.GenerateImageRequestVariant2).Name}");
-                        generateImageRequestVariant2 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                    }
-                    catch (global::System.Text.Json.JsonException)
-                    {
-                    }
-                    catch (global::System.InvalidOperationException)
-                    {
-                    }
-                }
+                var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Recraft.GenerateImageRequestVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Recraft.GenerateImageRequestVariant2> ??
+                               throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Recraft.GenerateImageRequestVariant2).Name}");
+                generateImageRequestVariant2 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
             }
-
-            if (@base == null && generateImageRequestVariant2 == null)
+            catch (global::System.Text.Json.JsonException)
             {
-                try
-                {
-
-                    var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Recraft.GenerateImageRequestBase), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Recraft.GenerateImageRequestBase> ??
-                                   throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Recraft.GenerateImageRequestBase).Name}");
-                    @base = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                }
-                catch (global::System.Text.Json.JsonException)
-                {
-                }
-                catch (global::System.InvalidOperationException)
-                {
-                }
             }
-
-            if (@base == null && generateImageRequestVariant2 == null)
+            catch (global::System.InvalidOperationException)
             {
-                try
-                {
-
-                    var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Recraft.GenerateImageRequestVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Recraft.GenerateImageRequestVariant2> ??
-                                   throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Recraft.GenerateImageRequestVariant2).Name}");
-                    generateImageRequestVariant2 = global::System.Text.Json.JsonSerializer.Deserialize(__rawJson, typeInfo);
-                }
-                catch (global::System.Text.Json.JsonException)
-                {
-                }
-                catch (global::System.InvalidOperationException)
-                {
-                }
             }
-
             var __value = new global::Recraft.GenerateImageRequest(
                 @base,
 
@@ -153,18 +63,46 @@ namespace Recraft.JsonConverters
             options = options ?? throw new global::System.ArgumentNullException(nameof(options));
             var typeInfoResolver = options.TypeInfoResolver ?? throw new global::System.InvalidOperationException("TypeInfoResolver is not set.");
 
+
+            writer.WriteStartObject();
+            var __writtenPropertyNames = new global::System.Collections.Generic.HashSet<string>(global::System.StringComparer.Ordinal);
             if (value.IsBase)
             {
                 var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Recraft.GenerateImageRequestBase), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Recraft.GenerateImageRequestBase?> ??
                                throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Recraft.GenerateImageRequestBase).Name}");
-                global::System.Text.Json.JsonSerializer.Serialize(writer, value.Base!, typeInfo);
+                var __element0 = global::System.Text.Json.JsonSerializer.SerializeToElement(value.Base!, typeInfo);
+                if (__element0.ValueKind != global::System.Text.Json.JsonValueKind.Object)
+                {
+                    throw new global::System.Text.Json.JsonException("AllOf values must serialize as JSON objects.");
+                }
+
+                foreach (var __property in __element0.EnumerateObject())
+                {
+                    if (__writtenPropertyNames.Add(__property.Name))
+                    {
+                        __property.WriteTo(writer);
+                    }
+                }
             }
-            else if (value.IsGenerateImageRequestVariant2)
+            if (value.IsGenerateImageRequestVariant2)
             {
                 var typeInfo = typeInfoResolver.GetTypeInfo(typeof(global::Recraft.GenerateImageRequestVariant2), options) as global::System.Text.Json.Serialization.Metadata.JsonTypeInfo<global::Recraft.GenerateImageRequestVariant2?> ??
                                throw new global::System.InvalidOperationException($"Cannot get type info for {typeof(global::Recraft.GenerateImageRequestVariant2).Name}");
-                global::System.Text.Json.JsonSerializer.Serialize(writer, value.GenerateImageRequestVariant2!, typeInfo);
+                var __element1 = global::System.Text.Json.JsonSerializer.SerializeToElement(value.GenerateImageRequestVariant2!, typeInfo);
+                if (__element1.ValueKind != global::System.Text.Json.JsonValueKind.Object)
+                {
+                    throw new global::System.Text.Json.JsonException("AllOf values must serialize as JSON objects.");
+                }
+
+                foreach (var __property in __element1.EnumerateObject())
+                {
+                    if (__writtenPropertyNames.Add(__property.Name))
+                    {
+                        __property.WriteTo(writer);
+                    }
+                }
             }
+            writer.WriteEndObject();
         }
     }
 }

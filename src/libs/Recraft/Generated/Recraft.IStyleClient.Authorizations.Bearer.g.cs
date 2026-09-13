1ab6efc4170c2777119a399045e453a5a4d01d2c
Konstantin S. <havendv@gmail.com>
2026-09-13 19:49:26 +0400

    feat: Updated OpenAPI spec (#168)
    Co-authored-by: github-actions[bot] <dependabot@bot.com>

---
+++ src/libs/Recraft/Generated/Recraft.IStyleClient.Authorizations.Bearer.g.cs
@@ -0,0 +1,16 @@
+
+#nullable enable
+
+namespace Recraft
+{
+    public partial interface IStyleClient
+    {
+        /// <summary>
+        /// Authorize using bearer authentication.
+        /// </summary>
+        /// <param name="apiKey"></param>
+
+        public void AuthorizeUsingBearer(
+            string apiKey);
+    }
+}
